733d238a5df3b58fe4da023138dfc1cb1e3d06c2
Ahmed Hasanaath <61200955+gufranSabri@users.noreply.github.com>
2024-06-05 23:17:41 +0300

    small fix

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def open_browser():
     webbrowser.open('http://127.0.0.1:4000')
 
 if __name__ == '__main__':
-    # threading.Timer(1.5, open_browser).start()
+    threading.Timer(1.5, open_browser).start()
     socketio.run(app, host="0.0.0.0", port=4000, debug=True)
